ccf540e96b12779485c5ff1690d394ab7a7f31b2
AleksGin <alexanderginin@icloud.com>
2024-10-15 21:59:08 +0500

    format code

--- bot/services/crossworld_service.py
+++ bot/services/crossworld_service.py
@@ -28,7 +28,9 @@ from message_service import MessageService
 
 class CrossworldService:
     def __init__(
-        self, message_service: MessageService, cross_table: CrossworldTableRepo
+        self,
+        message_service: MessageService,
+        cross_table: CrossworldTableRepo,
     ) -> None:
         self.message_service = message_service
         self.cross_table = cross_table
@@ -52,7 +54,9 @@ class CrossworldService:
 
     async def send_rules(self, message: Message) -> Message:
         await self.message_service.send_message(
-            message=message, text=Misc.instruction_text, keyboard=instruction_button()
+            message=message,
+            text=Misc.instruction_text,
+            keyboard=instruction_button(),
         )
         return await self.open_main(message=message)
 
@@ -102,12 +106,16 @@ class CrossworldService:
         return await self.open_main(message=message)
 
     async def open_main(
-        self, message: Message, state: FSMContext | None = None
+        self,
+        message: Message,
+        state: FSMContext | None = None,
     ) -> Message:
         if state:
             await state.clear()
         return await self.message_service.send_message(
-            message=message, text=MainMenu.main_menu, keyboard=welcome_keyboard()
+            message=message,
+            text=MainMenu.main_menu,
+            keyboard=welcome_keyboard(),
         )
 
     async def open_clothes_keyboard(self, message: Message) -> Message:
@@ -119,12 +127,19 @@ class CrossworldService:
             chat_id=message.chat.id,
         )
 
-    async def process_amount(self, message: Message, state: FSMContext) -> None:
+    async def process_amount(
+        self,
+        message: Message,
+        state: FSMContext,
+    ) -> None:
         user_data = await state.get_data()
         clothe_name = user_data.get("clothe_name")
 
         if clothe_name is None:
-            await self.message_service.send_message(message=message, text=Misc.wrong)
+            await self.message_service.send_message(
+                message=message,
+                text=Misc.wrong,
+            )
             return
 
         try:
@@ -141,14 +156,17 @@ class CrossworldService:
                         result = (amount * Misc.rate) + fixed_price
 
                     calculating_message = await self.message_service.send_message(
-                        message=message, text=Order.calculating_process_text
+                        message=message,
+                        text=Order.calculating_process_text,
                     )
                     await asyncio.sleep(delay=1.3)
                     await calculating_message.delete()
                     await self.message_service.send_message(
                         message=message,
                         text=Order.total_amount.format(
-                            int(result), delivery_price, comission
+                            int(result),
+                            delivery_price,
+                            comission,
                         ),
                     )
                     await self.message_service.send_message(
@@ -161,11 +179,13 @@ class CrossworldService:
                     )
                 else:
                     await self.message_service.send_message(
-                        message=message, text=Order.wrong_less_then_0_text
+                        message=message,
+                        text=Order.wrong_less_then_0_text,
                     )
         except ValueError:
             await self.message_service.send_message(
-                message=message, text=Order.wrong_value_text
+                message=message,
+                text=Order.wrong_value_text,
             )
 
     async def make_order_action(
@@ -183,25 +203,38 @@ class CrossworldService:
             )
 
     async def not_make_order_action(
-        self, callback: CallbackQuery, state: FSMContext
+        self,
+        callback: CallbackQuery,
+        state: FSMContext,
     ) -> None:
         await self.message_service.order_handle_callback(
-            callback=callback, action=self.open_main, state=state
+            callback=callback,
+            action=self.open_main,
+            state=state,
         )
 
     async def calculate_again_action(self, callback: CallbackQuery) -> None:
         await self.message_service.order_handle_callback(
-            callback=callback, action=self.open_clothes_keyboard
+            callback=callback,
+            action=self.open_clothes_keyboard,
         )
 
     async def callback_delivery_status(
-        self, callback: CallbackQuery, state: FSMContext
+        self,
+        callback: CallbackQuery,
+        state: FSMContext,
     ) -> None:
         await self.message_service.order_handle_callback(
-            callback=callback, action=self.delivery_status_action, state=state
+            callback=callback,
+            action=self.delivery_status_action,
+            state=state,
         )
 
-    async def delivery_status_action(self, message: Message, state: FSMContext) -> None:
+    async def delivery_status_action(
+        self,
+        message: Message,
+        state: FSMContext,
+    ) -> None:
         data = await state.get_data()
         if not data.get("photo_sent"):
             await self.message_service.send_message(
@@ -223,12 +256,15 @@ class CrossworldService:
         await state.set_state(state=OrderStatusState.waiting_order_number)
 
     async def delivery_status_process(
-        self, message: Message, state: FSMContext
+        self,
+        message: Message,
+        state: FSMContext,
     ) -> None:
         if message.text is not None and message.text.isdigit():
             order_number = message.text
             search_message = await self.message_service.send_message(
-                message=message, text=Order.search_order_text
+                message=message,
+                text=Order.search_order_text,
             )
             value = self.cross_table.search_delivery_status(data=order_number)
             await search_message.delete()
@@ -244,7 +280,10 @@ class CrossworldService:
             else:
                 await self.message_service.send_message(
                     message=message,
-                    text=Order.order_number_status_text.format(order_number, value),
+                    text=Order.order_number_status_text.format(
+                        order_number,
+                        value,
+                    ),
                 )
                 await self.message_service.send_message(
                     message=message,
@@ -256,5 +295,6 @@ class CrossworldService:
                 )
         else:
             await self.message_service.send_message(
-                message=message, text=Order.wrong_value_order_number_text
+                message=message,
+                text=Order.wrong_value_order_number_text,
             )
