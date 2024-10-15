f11b4cdd5925640088313e2aa13254157c2f09bb
AleksGin <alexanderginin@icloud.com>
2024-10-15 15:05:11 +0500

    add BotConfig, TableConfig to config_reader

---
+++ bot/core/config_reader.py
@@ -0,0 +1,25 @@
+from pydantic import BaseModel, SecretStr
+from pydantic_settings import BaseSettings, SettingsConfigDict
+
+
+class BotConfig(BaseModel):
+    token_bot: SecretStr
+
+
+class TableConfig(BaseModel):
+    cred_file: SecretStr
+    url_table: str
+
+
+class Settings(BaseSettings):
+    bot_config: BotConfig
+    table_config: TableConfig
+
+    model_config = SettingsConfigDict(
+        env_file=(".env.template", ".env"),
+        env_file_encoding="utf-8",
+        env_nested_delimiter="__",
+    )
+
+
+config = Settings()
