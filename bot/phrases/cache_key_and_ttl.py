92a58028143fb66f553454c5a6742895de8367f7
AleksGin <alexanderginin@icloud.com>
2024-10-25 16:52:10 +0500

    make another form for USER_ID_KEY

--- bot/phrases/cache_key_and_ttl.py
+++ bot/phrases/cache_key_and_ttl.py
@@ -1,8 +1,9 @@
 class CacheKey:
-    USER_ID_KEY: str = "user_id_{}_order_id_{}_info:"
+    USER_ID_KEY: str = "user:{}:order:{}"
 
 
 class CacheTTL:
     hour: int = 3600
     day: int = 86400
     week: int = day * 7
+
