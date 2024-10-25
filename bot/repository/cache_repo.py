f5e28806fc7b044b3bcef15aec4287ee881fee57
AleksGin <alexanderginin@icloud.com>
2024-10-25 16:45:58 +0500

    add new method to CacheRepo for get all keys

--- bot/repository/cache_repo.py
+++ bot/repository/cache_repo.py
@@ -44,3 +44,20 @@ class CacheRepo:
             if info is None:
                 return None
             return info.decode("utf-8")
+
+    async def get_all_cached_orders(self):
+        async with Redis.from_pool(connection_pool=self.pool) as redis:
+            cursor = 0
+            keys = []
+
+            while True:
+                cursor, found_keys = await redis.scan(
+                    cursor=cursor,
+                    match="user:*:order:*",
+                )
+                keys.extend(found_keys)
+
+                if cursor == 0:
+                    break
+
+            return keys
