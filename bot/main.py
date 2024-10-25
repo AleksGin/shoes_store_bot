9b7ff5269111c9365ea973eedda9e8b618aa4fa8
AleksGin <alexanderginin@icloud.com>
2024-10-25 16:55:05 +0500

    add AsyncGoogleSheetsService to main and add RedisStorage for state data

--- bot/main.py
+++ bot/main.py
@@ -3,6 +3,7 @@ import logging
 import sys
 
 import redis.asyncio as redis
+from aiogram.fsm.storage.redis import RedisStorage
 from aiogram import Bot, Dispatcher
 from aiogram.client.default import DefaultBotProperties
 from aiogram.enums import ParseMode
@@ -13,12 +14,14 @@ from middlewares import ServiceMiddleware
 from repository import (
     CacheRepo,
     CrossworldTableRepo,
+    AsyncGoogleSheetsService,
 )
 from services import (
     CrossworldService,
     MessageService,
     UpdateDataService,
 )
+from phrases import CacheTTL
 
 
 async def main() -> None:
@@ -30,28 +33,41 @@ async def main() -> None:
     pool = redis.ConnectionPool.from_url(
         url=config.cache_config.url,
     )
+
+    redis_client = await redis.from_url(config.cache_config.url)
+
     cache_repo = CacheRepo(pool=pool)
+
+    storage = RedisStorage(
+        redis=redis_client,
+        state_ttl=CacheTTL.week,
+        data_ttl=CacheTTL.week,
+    )
 
     cross_table = CrossworldTableRepo(
         cred_file=config.table_config.cred_file.get_secret_value(),
         sheet_url=config.table_config.url_table,
     )
+
+    async_table = AsyncGoogleSheetsService(sheets_service=cross_table)
+
     message_service = MessageService(
         bot=bot,
     )
     cross_service = CrossworldService(
         message_service=message_service,
-        cross_table=cross_table,
         cache_repo=cache_repo,
+        async_table=async_table,
     )
     update_service = UpdateDataService(
-        cross_service=cross_service,
         cache_repo=cache_repo,
+        cross_service=cross_service,
+        async_table=async_table,
     )
 
     update_service.start_scheduler()
 
-    dp = Dispatcher()
+    dp = Dispatcher(storage=storage)
 
     dp.message.middleware(
         ServiceMiddleware(
@@ -60,6 +76,7 @@ async def main() -> None:
             cross_table=cross_table,
             cache_repo=cache_repo,
             update_service=update_service,
+            async_table=async_table,
         )
     )
     dp.callback_query.middleware(
@@ -69,6 +86,7 @@ async def main() -> None:
             cross_table=cross_table,
             cache_repo=cache_repo,
             update_service=update_service,
+            async_table=async_table,
         )
     )
 
