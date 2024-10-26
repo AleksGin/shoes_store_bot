7eebdfbab712f3db645f4086dd57eefd6a0fddc7
AleksGin <alexanderginin@icloud.com>
2024-10-26 13:44:25 +0500

    some refactor and add new button

--- bot/keyboards/Inline/keyboard_ask_for_order.py
+++ bot/keyboards/Inline/keyboard_ask_for_order.py
@@ -10,14 +10,32 @@ def ask_for_order_keyboard() -> InlineKeyboardMarkup:
     builder = InlineKeyboardBuilder()
 
     builder.row(
-        InlineKeyboardButton(text=Order.agree_order_text, callback_data="make_order"),
         InlineKeyboardButton(
-            text=Order.not_agree_order_text, callback_data="not_make_order"
+            text=Order.agree_order_text,
+            callback_data="make_order",
+        ),
+        InlineKeyboardButton(
+            text=Order.not_agree_order_text,
+            callback_data="not_make_order",
         ),
     )
     builder.row(
         InlineKeyboardButton(
-            text=Order.calculate_again_text, callback_data="calculate_again"
+            text=Order.calculate_again_text,
+            callback_data="calculate_again",
+        ),
+    )
+
+    return builder.as_markup(resize_keyboard=True)
+
+
+def ask_for_track_order() -> InlineKeyboardMarkup:
+    builder = InlineKeyboardBuilder()
+
+    builder.row(
+        InlineKeyboardButton(
+            text=Order.ask_for_track_text,
+            callback_data="tracking_on,",
         ),
     )
 
