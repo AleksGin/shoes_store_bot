51be7e91a49f34641591ee467e55f1a7a9cd3213
AleksGin <alexanderginin@icloud.com>
2024-10-28 09:11:07 +0500

    expanded the capabilities of the method: now it searches by match_form

--- bot/repository/cache_repo.py
+++ bot/repository/cache_repo.py
@@ -1,3 +1,6 @@
+import asyncio
+import logging
+
 from phrases import (
     CacheKey,
     CacheTTL,
@@ -18,34 +21,56 @@ class CacheRepo:
         order_number: str,
         info: str,
         expire_time: int = CacheTTL.day,
+        tracking: bool = False,
     ) -> None:
         async with Redis.from_pool(connection_pool=self.pool) as redis:
-            key: str = CacheKey.USER_ID_KEY.format(
-                user_id,
-                order_number,
-            )
-            await redis.set(
+            if tracking:
+                logging.info("понял, что tracking True и я выполняю внутри tracking")
+                key: str = CacheKey.TRACKING_USER_ID_KEY.format(
+                    user_id,
+                    order_number,
+                )
+            else:
+                key: str = CacheKey.USER_ID_KEY.format(
+                    user_id,
+                    order_number,
+                )
+            setting = await redis.set(
                 name=key,
                 value=info,
                 ex=expire_time,
             )
+            logging.info(
+                f"форма ключа: {key}, инфо: {info}, а сама функция вот так: {setting}"
+            )
 
     async def get_info_about_order_by_user_id(
-        self,
-        user_id: int,
-        order_number: str,
+        self, user_id: int, order_number: str, tracking: bool = False
     ):
         async with Redis.from_pool(connection_pool=self.pool) as redis:
-            key = CacheKey.USER_ID_KEY.format(
-                user_id,
-                order_number,
-            )
+            if tracking:
+                await asyncio.sleep(4)
+                key: str = CacheKey.TRACKING_USER_ID_KEY.format(
+                    user_id,
+                    order_number,
+                )
+            else:
+                await asyncio.sleep(4)
+                key = CacheKey.USER_ID_KEY.format(
+                    user_id,
+                    order_number,
+                )
             info = await redis.get(name=key)
+            await asyncio.sleep(4)
             if info is None:
                 return None
             return info.decode("utf-8")
 
-    async def get_all_cached_orders(self):
+    async def get_all_cached_orders(
+        self,
+        match_form: str,
+    ):
+        await asyncio.sleep(4)
         async with Redis.from_pool(connection_pool=self.pool) as redis:
             cursor = 0
             keys = []
@@ -53,7 +78,7 @@ class CacheRepo:
             while True:
                 cursor, found_keys = await redis.scan(
                     cursor=cursor,
-                    match="user:*:order:*",
+                    match=match_form,
                 )
                 keys.extend(found_keys)
 
