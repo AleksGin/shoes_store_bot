dbda8e2711c09ff40785098443d55be8631ad2c0
AleksGin <alexanderginin@icloud.com>
2024-10-28 08:50:23 +0500

    add new forms in CacheKey

--- bot/phrases/cache_key_and_ttl.py
+++ bot/phrases/cache_key_and_ttl.py
@@ -1,9 +1,14 @@
 class CacheKey:
     USER_ID_KEY: str = "user:{}:order:{}"
+    TRACKING_USER_ID_KEY: str = "tracking_user:{}:order:{}"
+    match_for_key: str = USER_ID_KEY.format("*", "*")
+    match_tracking_key: str = TRACKING_USER_ID_KEY.format("*", "*")
 
 
 class CacheTTL:
     hour: int = 3600
     day: int = 86400
     week: int = day * 7
+    
+
 
