8d9f241854cb03ea5ff8b0d82fd13ff5c152befc
AleksGin <alexanderginin@icloud.com>
2024-10-30 13:39:12 +0500

    create new states to OrderStatusState

--- bot/fsm/order_status_state.py
+++ bot/fsm/order_status_state.py
@@ -6,4 +6,6 @@ from aiogram.fsm.state import (
 
 class OrderStatusState(StatesGroup):
     waiting_order_number = State()
-    waiting_for_view_new_order_button = State()
+    waiting_order_number_to_delete = State()
+    waiting_for_press_tracker_button = State()
+    all_tracker_deleted_notification = State()
