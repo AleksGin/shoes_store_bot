4608742fecc55a98c6e9769258a965f868f0c446
AleksGin <alexanderginin@icloud.com>
2024-10-30 13:38:06 +0500

    add new method to CacheRepo

--- bot/repository/cache_repo.py
+++ bot/repository/cache_repo.py
@@ -45,23 +45,23 @@ class CacheRepo:
             )
 
     async def get_info_about_order_by_user_id(
-        self, user_id: int, order_number: str, tracking: bool = False
+        self,
+        user_id: int,
+        order_number: str,
+        tracking: bool = False,
     ):
         async with Redis.from_pool(connection_pool=self.pool) as redis:
             if tracking:
-                await asyncio.sleep(4)
                 key: str = CacheKey.TRACKING_USER_ID_KEY.format(
                     user_id,
                     order_number,
                 )
             else:
-                await asyncio.sleep(4)
                 key = CacheKey.USER_ID_KEY.format(
                     user_id,
                     order_number,
                 )
             info = await redis.get(name=key)
-            await asyncio.sleep(4)
             if info is None:
                 return None
             return info.decode("utf-8")
@@ -70,7 +70,6 @@ class CacheRepo:
         self,
         match_form: str,
     ):
-        await asyncio.sleep(4)
         async with Redis.from_pool(connection_pool=self.pool) as redis:
             cursor = 0
             keys = []
@@ -86,3 +85,34 @@ class CacheRepo:
                     break
 
             return keys
+
+    async def get_multiple_order_infos(self, keys):
+        async with Redis.from_pool(connection_pool=self.pool) as redis:
+            infos = await redis.mget(keys=keys)
+
+        return [info.decode("utf-8") if info else None for info in infos]
+
+    async def delete_tracking_orders(
+        self,
+        user_id: int,
+        order_number: str | None = None,
+        single_order_only: bool = False,
+    ):
+        logging.info("перешел в delete_tracking_orders")
+        async with Redis.from_pool(connection_pool=self.pool) as redis:
+            if single_order_only:
+                logging.info(f"обратился {single_order_only}")
+                result = await redis.delete(
+                    CacheKey.TRACKING_USER_ID_KEY.format(
+                        user_id,
+                        order_number,
+                    )
+                )
+                return result > 0
+            else:
+                orders = await self.get_all_cached_orders(
+                    match_form=CacheKey.match_for_tracking_orders.format(user_id)
+                )
+                if orders:
+                    await redis.delete(*orders)
+        return False
