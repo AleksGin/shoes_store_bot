6f7fdcc8b7639fa6297e8e1ab33453328f0c5c93
AleksGin <alexanderginin@icloud.com>
2024-11-06 18:16:36 +0300

    .

--- bot/services/__init__.py
+++ bot/services/__init__.py
@@ -7,8 +7,8 @@ __all__ = (
 )
 
 
-from .crossworld_service import CrossworldService
-from .message_service import MessageService
-from .data_update_service import UpdateDataService
 from .cache_service import CacheService
+from .crossworld_service import CrossworldService
+from .data_update_service import UpdateDataService
+from .message_service import MessageService
 from .order_service import OrderService
