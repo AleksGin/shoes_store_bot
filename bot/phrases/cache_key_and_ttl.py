4ff90570969dc2addca7ffb27bf02511f22b7a53
AleksGin <alexanderginin@icloud.com>
2024-11-06 08:49:47 +0300

    rename and add new match_form for CacheKey class

--- bot/phrases/cache_key_and_ttl.py
+++ bot/phrases/cache_key_and_ttl.py
@@ -3,7 +3,8 @@ class CacheKey:
     TRACKING_USER_ID_KEY: str = "tracking_user:{}:order:{}"
     match_for_key: str = ORDER_STATUS_KEY.format("*")
     match_tracking_key: str = TRACKING_USER_ID_KEY.format("*", "*")
-    match_for_tracking_orders: str = "tracking_user:{}:order:*"
+    match_for_user_to_orders: str = "tracking_user:{}:order:*"
+    match_for_order_to_users: str = "tracking_user:*order:{}"
 
 
 class CacheTTL:
