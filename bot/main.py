d88058899c10f9bc0dc1bf3d7c36941121f56cf6
AleksGin <alexanderginin@icloud.com>
2024-11-06 18:04:49 +0300

    refactor main.py

--- bot/main.py
+++ bot/main.py
@@ -2,68 +2,26 @@ import asyncio
 import logging
 import sys
 
-import redis.asyncio as redis
-from aiogram.fsm.storage.redis import RedisStorage
-from aiogram import Bot, Dispatcher
-from aiogram.client.default import DefaultBotProperties
-from aiogram.enums import ParseMode
+from aiogram import Dispatcher
 from callbacks.callbacks import router as callback_router
-from core.config_reader import config
+from core import ServiceFactory
 from handlers.handlers import router as handler_router
 from middlewares import ServiceMiddleware
-from repository import (
-    CacheRepo,
-    CrossworldTableRepo,
-    AsyncGoogleSheetsService,
-)
-from services import (
-    CrossworldService,
-    MessageService,
-    UpdateDataService,
-)
-from phrases import CacheTTL
 
 
 async def main() -> None:
-    bot = Bot(
-        token=config.bot_config.token_bot.get_secret_value(),
-        default=DefaultBotProperties(parse_mode=ParseMode.HTML),
-    )
-
-    pool = redis.ConnectionPool.from_url(
-        url=config.cache_config.url,
-    )
-
-    redis_client = await redis.from_url(url=config.cache_config.url)
-
-    cache_repo = CacheRepo(pool=pool, redis=redis_client)
-
-    storage = RedisStorage(
-        redis=redis_client,
-        state_ttl=CacheTTL.week,
-        data_ttl=CacheTTL.week,
-    )
-
-    cross_table = CrossworldTableRepo(
-        cred_file=config.table_config.cred_file.get_secret_value(),
-        sheet_url=config.table_config.url_table,
-    )
-
-    async_table = AsyncGoogleSheetsService(sheets_service=cross_table)
-
-    message_service = MessageService(
-        bot=bot,
-    )
-    cross_service = CrossworldService(
-        message_service=message_service,
-        cache_repo=cache_repo,
-        async_table=async_table,
-    )
-    update_service = UpdateDataService(
-        cache_repo=cache_repo,
-        cross_service=cross_service,
-        async_table=async_table,
-    )
+    (
+        bot,
+        cache_repo,
+        storage,
+        message_service,
+        cross_table,
+        async_table,
+        order_service,
+        cache_service,
+        cross_service,
+        update_service,
+    ) = await ServiceFactory.create_services()
 
     update_service.start_scheduler()
 
@@ -77,6 +35,8 @@ async def main() -> None:
             cache_repo=cache_repo,
             update_service=update_service,
             async_table=async_table,
+            order_service=order_service,
+            cache_service=cache_service,
         )
     )
     dp.callback_query.middleware(
@@ -87,6 +47,8 @@ async def main() -> None:
             cache_repo=cache_repo,
             update_service=update_service,
             async_table=async_table,
+            order_service=order_service,
+            cache_service=cache_service,
         )
     )
 
