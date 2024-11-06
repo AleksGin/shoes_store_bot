331454861a75ebac2e09ef1bf49c528adbb4e28c
AleksGin <alexanderginin@icloud.com>
2024-11-06 18:01:19 +0300

    add CacheService and OrderService to export

--- bot/services/__init__.py
+++ bot/services/__init__.py
@@ -1,10 +1,14 @@
 __all__ = (
     "CrossworldService",
     "MessageService",
-    "UpdateDataService"
+    "UpdateDataService",
+    "CacheService",
+    "OrderService",
 )
 
 
 from .crossworld_service import CrossworldService
 from .message_service import MessageService
 from .data_update_service import UpdateDataService
+from .cache_service import CacheService
+from .order_service import OrderService
